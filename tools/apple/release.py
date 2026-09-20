6036cdb1da5e627299c40a9d13c55bed52fbc64c
GT <gregjotau@gmail.com>
2026-09-20 13:53:27 +0200

    Resolve signing certificate chain in temporary keychain

--- tools/apple/release.py
+++ tools/apple/release.py
@@ -7,6 +7,7 @@ from pathlib import Path
 import plistlib
 import re
 import secrets
+import shlex
 import subprocess
 import tempfile
 
@@ -33,8 +34,10 @@ def release(app_id, version):
         key.chmod(0o600)
         keychain = work / 'signing.keychain-db'
         password = secrets.token_urlsafe(32)
+        original_keychains = shlex.split(output('security', 'list-keychains', '-d', 'user'))
         run('security', 'create-keychain', '-p', password, keychain)
         try:
+            run('security', 'list-keychains', '-d', 'user', '-s', keychain, *original_keychains)
             run('security', 'set-keychain-settings', '-lut', '21600', keychain)
             run('security', 'unlock-keychain', '-p', password, keychain)
             run('security', 'import', certificate, '-P', os.environ['APPLE_SIGNING_PASSWORD'], '-k', keychain, '-T', '/usr/bin/codesign', capture_output=True)
@@ -79,7 +82,10 @@ def release(app_id, version):
             checksum(image)
             print(f'Ready: {image}')
         finally:
-            run('security', 'delete-keychain', keychain)
+            try:
+                run('security', 'list-keychains', '-d', 'user', '-s', *original_keychains)
+            finally:
+                run('security', 'delete-keychain', keychain)
 
 
 if __name__ == '__main__':
